8815471566534e1b9d141d8104e58f49ee4a2e9f
chanchal.k <mailing.chanchal@gmail.com>
2018-07-31 16:24:02 +0530

    EmsAutomation framework

---
+++ src/main/java/pages/basePage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
+
+public class basePage {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+
+    //Constructor
+    public basePage (WebDriver driver, WebDriverWait wait){
+
+        this.driver = driver;
+        this.wait = wait;
+
+
+    }
+
+    //Click Method
+    public void click (By elementLocation) {
+        driver.findElement(elementLocation).click();
+    }
+
+    //Write Text
+    public void writeText (By elementLocation, String text) {
+        driver.findElement(elementLocation).sendKeys(text);
+    }
+
+    //Read Text
+    public String readText (By elementLocation) {
+        return driver.findElement(elementLocation).getText();
+    }
+
+    //Enter button press
+    public void enterButton (By elementLocation) {
+        driver.findElement(elementLocation).sendKeys(Keys.RETURN);
+    }
+
+}
