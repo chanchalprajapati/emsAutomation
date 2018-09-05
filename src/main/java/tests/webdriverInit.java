6098add51de0759db7739fda2d2e3bc7c27c3a89
chanchal.k <mailing.chanchal@gmail.com>
2018-09-05 12:42:28 +0530

    Excell changes

--- src/main/java/tests/webdriverInit.java
+++ src/main/java/tests/webdriverInit.java
@@ -1,14 +1,17 @@
 package tests;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import uitilities.genric;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class webdriverInit {
@@ -49,6 +52,7 @@ public class webdriverInit {
                 + "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
         return driver;
     }
@@ -58,27 +62,44 @@ public class webdriverInit {
         System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
-
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         return driver;
 
 
     }
 
 
-    @BeforeTest
+    @BeforeMethod
         public void setup () {
 
         setDriver(1);
 
 
 
+
     }
 
 
 
 
-    @AfterTest
-    public void teardown () {
+   @AfterMethod
+    public void teardown (ITestResult result) {
+
+
+            if (ITestResult.FAILURE == result.getStatus()){
+                try {
+                    TakesScreenshot screenshot = (TakesScreenshot)driver;
+                    File src = screenshot.getScreenshotAs(OutputType.FILE);
+                    FileUtils.copyFile(src, new File("C:\\emsAutomation\\src\\main\\resources\\Screenshoots\\"+ genric.getCurrentDateTime()+".png"));
+                    System.out.println("Successfully captured the screenshot");
+                }
+                catch (Exception e){
+                    System.out.println("Exception while taking screenshot "+e.getMessage());
+                }
+            }
         driver.quit();
+        }
+
+
     }
-}
+
